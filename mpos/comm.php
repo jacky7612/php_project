c0efc3b03f5b0a1f005dcdb18a9eeda35e7270ef
jacky <k761233@gmail.com>
2022-10-25 15:31:31 +0800

    補檔，更新規格

---
+++ mpos/comm.php
@@ -0,0 +1,11 @@
+<?php
+	//$g_mpost_url = "https://mposuat.transglobe.com.tw:1443/global-server2/"; //開發機
+	
+//	$g_mpost_url = "http://10.67.67.53/global-server2/"; //SIT
+//	$g_mpost_url = "http://10.67.67.52:8080/global-server2/";//UAT
+	$g_mpost_url = "http://10.67.65.75:8080/global-server2/";//PROD
+	
+	
+	
+	
+?>
